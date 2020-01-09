f20edf0aeae922038cda0cb76e12dda1f3ab8684
mukeshdass <mukesh95dass@gmail.com>
2020-01-09 12:40:39 +0530

    Completed

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,13 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		// TODO Auto-generated method stub
+System.out.println("My company name is TCS");
+	}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+	}
+}
